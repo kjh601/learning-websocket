bb41ed4986609fe586cb92b1f6faaa014ea7a173
Algorithm <fighter0628@gmail.com>
2024-03-21 22:13:19 +0900

    Chore: 사용되지 않는 어노테이션 제거

--- src/main/java/learning/websocket/dto/MessageDto.java
+++ src/main/java/learning/websocket/dto/MessageDto.java
@@ -1,11 +1,9 @@
 package learning.websocket.dto;
 
 import learning.websocket.enums.MessageType;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class MessageDto {
     private MessageType messageType;
     private Long chatRoomId;
