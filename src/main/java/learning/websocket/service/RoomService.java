544a60e21eee3476a2de181009b1c3aace90af41
Algorithm <fighter0628@gmail.com>
2024-04-09 14:36:03 +0900

    feat: RoomService 메서드 수정 및 추가
    - createRoom : 새로운 userRoom 객체 생성 후 UserRoom 테이블에 추가하기, Room 테이블의 currentMembers 증가시키기
    
    - joinRoom : 채팅방 입장시 해당 채팅방에 유저 입장하였다는 메시지 보내는 로직 추가, Room 테이블의 currentMembers 증가시키는 로직 추가
    
    - leaveRoom : 채팅방 퇴장시 해당 채팅방에 유저 퇴장하였다는 메시지 보내는 로직 추가, Room 테이블의 currentMembers 감소시키는 로직 추가
    
    - findAll : 모든 채팅방 목록 조회하는 메서드 추가
    
    - updateRoom : 채팅방 정보 수정하는 메서드 추가

--- src/main/java/learning/websocket/service/RoomService.java
+++ src/main/java/learning/websocket/service/RoomService.java
@@ -1,19 +1,24 @@
 package learning.websocket.service;
 
 import jakarta.transaction.Transactional;
+import learning.websocket.converter.MessageConverter;
 import learning.websocket.converter.RoomConverter;
 import learning.websocket.converter.UserRoomConverter;
-import learning.websocket.dto.MessageLogDto;
+import learning.websocket.dto.MessageDto;
 import learning.websocket.dto.RoomDto;
-import learning.websocket.dto.UserRoomDto;
+import learning.websocket.dto.RoomWithParticipantStatusDto;
+import learning.websocket.entity.Message;
 import learning.websocket.entity.Room;
 import learning.websocket.entity.User;
 import learning.websocket.entity.UserRoom;
+import learning.websocket.enums.MessageType;
+import learning.websocket.enums.UserRole;
 import learning.websocket.repository.MessageRepository;
 import learning.websocket.repository.RoomRepository;
 import learning.websocket.repository.UserRepository;
 import learning.websocket.repository.UserRoomRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -30,36 +35,106 @@ public class RoomService {
     private final RoomRepository roomRepository;
     private final RoomConverter roomConverter;
 
+    private final MessageConverter messageConverter;
+
+    private final SimpMessagingTemplate template;
     private final MessageRepository messageRepository;
 
-    public Room createRoom(RoomDto.createReq request) {
-        Room room = roomConverter.toRoom(request);
-        return roomRepository.save(room);
+    @Transactional
+    public RoomDto.roomRes createRoom(RoomDto.createReq request, User user) {
+        Room room = roomConverter.toCreateRoom(request);
+        Room savedRoom = roomRepository.save(room);
+
+        UserRoom userRoom = UserRoom.builder()
+                .user(user)
+                .userRole(UserRole.ADMIN)
+                .room(savedRoom)
+                .build();
+        userRoomRepository.save(userRoom);
+        roomRepository.increaseCurrentMembers(room.getId());
+
+        return roomConverter.toRoomDto(savedRoom);
     }
 
-    public void removeRoom(RoomDto.removeReq request) {
-        roomRepository.deleteById(request.getId());
+    public void removeRoom(Long roomId) {
+        roomRepository.deleteById(roomId);
     }
 
     @Transactional
-    public UserRoom joinRoom(UserRoomDto.joinReq request) {
-        User user = userRepository.findById(request.getUserId()).orElseThrow();
-        Room room = roomRepository.findById(request.getRoomId()).orElseThrow();
+    public void joinRoom(Long userId, Long roomId) {
+        User user = userRepository.findById(userId).orElseThrow();
+        Room room = roomRepository.findById(roomId).orElseThrow();
+        UserRoom userRoom = userRoomConverter.toUserRoom(user, room, UserRole.MEMBER);
 
-        UserRoom userRoom = userRoomConverter.toUserRoom(user, room, request.getUserRole());
+        String destination = "/sub/rooms/" + roomId;
 
-        return userRoomRepository.save(userRoom);
+        MessageDto.saveReq notification = MessageDto.saveReq.builder()
+                .messageType(MessageType.ENTER)
+                .senderId(userId)
+                .senderName(user.getName())
+                .message(user.getName()+"씨가 입장하셨습니다.")
+                .build();
+
+        template.convertAndSend(destination, notification);
+
+        Message message = messageConverter.toMessage(notification, user, room);
+
+
+        messageRepository.save(message);
+        roomRepository.increaseCurrentMembers(roomId);
+        userRoomRepository.save(userRoom);
     }
 
     @Transactional
-    public void leaveRoom(UserRoomDto.leaveReq request) {
-        User user = userRepository.findById(request.getUserId()).orElseThrow();
-        Room room = roomRepository.findById(request.getRoomId()).orElseThrow();
+    public void leaveRoom(Long userId, Long roomId) {
+        User user = userRepository.findById(userId).orElseThrow();
+        Room room = roomRepository.findById(roomId).orElseThrow();
         UserRoom userRoom = userRoomRepository.findByUserAndRoom(user, room).orElseThrow();
+
+        String destination = "/sub/rooms/" + roomId;
+
+        MessageDto.saveReq notification = MessageDto.saveReq.builder()
+                .messageType(MessageType.LEAVE)
+                .senderId(userId)
+                .senderName(user.getName())
+                .message(user.getName()+"씨가 퇴장하셨습니다.")
+                .build();
+
+        template.convertAndSend(destination, notification);
+
+        Message message = messageConverter.toMessage(notification, user, room);
+
+
+        messageRepository.save(message);
+        roomRepository.decreaseCurrentMembers(room.getId());
         userRoomRepository.delete(userRoom);
     }
 
-    public List<MessageLogDto> getChatRoomMessages(Long roomId) {
-        return messageRepository.findMessageLogs(roomId);
+    public List<RoomWithParticipantStatusDto> findAll(Long userId) {
+        return roomRepository.findAllRoomsAndParticipationStatus(userId);
+    }
+
+    public RoomDto.roomRes findRoom(Long roomId) {
+
+        Room room = roomRepository.findById(roomId).orElseThrow();
+        return roomConverter.toRoomDto(room);
+    }
+
+    public RoomWithParticipantStatusDto findRoom(Long roomId, Long userId) {
+
+        Room room = roomRepository.findById(roomId).orElseThrow();
+        User user = userRepository.findById(userId).orElseThrow();
+        Boolean isParticipating = userRoomRepository.findByUserAndRoom(user, room).isPresent();
+        return roomConverter.toRoomDto(room, isParticipating);
+    }
+
+    public RoomDto.roomRes updateRoom(RoomDto.editReq request, Long roomId) {
+        Room findRoom = roomRepository.findById(roomId).orElseThrow();
+        Integer findRoomCurrentMembers = findRoom.getCurrentMembers();
+
+        Room room = roomConverter.toEditRoom(request, roomId, findRoomCurrentMembers);
+
+        Room savedRoom = roomRepository.save(room);
+        return roomConverter.toRoomDto(savedRoom);
     }
 }
