b069c7464795278a4d241c22536a7f0b6581ffd2
Algorithm <fighter0628@gmail.com>
2024-04-09 14:18:42 +0900

    refactor: MessageService 수정
    - save 메서드에서 Message 엔티티 만드는 작업을 MessageConverter를 통해 생성하도록 변경함
    
    - 메시지 기록을 가져오는 메서드를 구현함

--- src/main/java/learning/websocket/service/MessageService.java
+++ src/main/java/learning/websocket/service/MessageService.java
@@ -1,38 +1,46 @@
 package learning.websocket.service;
 
+import jakarta.transaction.Transactional;
+import learning.websocket.converter.MessageConverter;
 import learning.websocket.dto.MessageDto;
 import learning.websocket.entity.Message;
 import learning.websocket.entity.Room;
 import learning.websocket.entity.User;
-import learning.websocket.entity.UserRoom;
 import learning.websocket.repository.MessageRepository;
 import learning.websocket.repository.RoomRepository;
 import learning.websocket.repository.UserRepository;
-import learning.websocket.repository.UserRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class MessageService {
 
     private final MessageRepository messageRepository;
-    private final UserRoomRepository userRoomRepository;
+    private final MessageConverter messageConverter;
+
     private final UserRepository userRepository;
     private final RoomRepository roomRepository;
 
-    public void save(MessageDto messageDto, Long roomId) {
+    public void save(MessageDto.saveReq saveReq, Long roomId) {
 
-        User user = userRepository.findById(messageDto.getSenderId()).orElseThrow();
+        User user = userRepository.findById(saveReq.getSenderId()).orElseThrow();
         Room room = roomRepository.findById(roomId).orElseThrow();
-        UserRoom userRoom = userRoomRepository.findByUserAndRoom(user, room).orElseThrow();
 
-        Message message = Message.builder()
-                .userRoom(userRoom)
-                .messageType(messageDto.getMessageType())
-                .content(messageDto.getMessage())
-                .build();
+        Message message = messageConverter.toMessage(saveReq, user, room);
 
         messageRepository.save(message);
+    }
+
+    @Transactional
+    public List<MessageDto.logRes> getMessages(Long roomId, Long userId) {
+        Room findRoom = roomRepository.findById(roomId).orElseThrow();
+
+        return messageRepository.findAllByRoom(findRoom).stream()
+                .map(room -> messageConverter.toLogRes(room, userId))
+                .collect(Collectors.toList());
     }
 }
