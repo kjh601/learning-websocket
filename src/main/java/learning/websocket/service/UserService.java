82d6a639559fb9e31ea969b707db6ab13dc86c5a
Algorithm <fighter0628@gmail.com>
2024-04-09 14:44:54 +0900

    feat: 로그인 기능 추가

--- src/main/java/learning/websocket/service/UserService.java
+++ src/main/java/learning/websocket/service/UserService.java
@@ -1,10 +1,15 @@
 package learning.websocket.service;
 
+import jakarta.transaction.Transactional;
 import learning.websocket.converter.UserConverter;
 import learning.websocket.dto.UserDto;
 import learning.websocket.entity.User;
+import learning.websocket.entity.UserRoom;
+import learning.websocket.enums.UserRole;
 import learning.websocket.repository.UserRepository;
+import learning.websocket.repository.UserRoomRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,14 +18,28 @@ public class UserService {
     private final UserRepository userRepository;
     private final UserConverter userConverter;
 
-    public User joinUser(UserDto.joinReq req){
+    private final BCryptPasswordEncoder encoder;
+    private final UserRoomRepository userRoomRepository;
 
-        User user = userConverter.toUser(req);
-
-        return userRepository.save(user);
+    public UserDto.userRes findUser(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow();
+        return userConverter.toUserRes(user);
     }
 
-    public void removeUser(UserDto.removeReq req){
-        userRepository.deleteById(req.getId());
+    public void signIn(String userId, String name, String password) {
+        User user = User.builder()
+                .loginId(userId)
+                .name(name)
+                .password(encoder.encode(password))
+                .build();
+
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public UserDto.userRes findAdmin(Long roomId) {
+        UserRoom userRoom = userRoomRepository.findByRoomIdAndUserRole(roomId, UserRole.ADMIN).orElseThrow();
+        User user = userRoom.getUser();
+        return userConverter.toUserRes(user);
     }
 }
