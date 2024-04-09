2ede9603dd5c90dcf188993c2b8d8285b97b1f2d
Algorithm <fighter0628@gmail.com>
2024-04-09 14:20:28 +0900

    feat: 로그인 기능 추가

---
+++ src/main/java/learning/websocket/config/PrincipalDetails.java
@@ -0,0 +1,63 @@
+package learning.websocket.config;
+
+import learning.websocket.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class PrincipalDetails implements UserDetails {
+
+    private User user;
+
+    public PrincipalDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collections = new ArrayList<>();
+        collections.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return null;
+            }
+        });
+        return collections;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getLoginId();
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
