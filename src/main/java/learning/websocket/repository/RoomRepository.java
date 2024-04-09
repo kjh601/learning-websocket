da03c3faa60d7bad26d76a7143e8a5e70782beab
Algorithm <fighter0628@gmail.com>
2024-04-09 14:27:36 +0900

    feat: RoomRepository 메서드 추가
    increaseCurrentMembers : 채팅방 입장 시 채팅방 인원 증가하는 메서드
    decreaseCurrentMembers : 채팅방 퇴장 시 채팅방 인원 감소하는 메서드
    findAllRoomsAndParticipationStatus : 모든 채팅방 목록을 찾을 때 유저의 참여 유무 상태를 나타내는 속성을 포함

--- src/main/java/learning/websocket/repository/RoomRepository.java
+++ src/main/java/learning/websocket/repository/RoomRepository.java
@@ -1,8 +1,25 @@
 package learning.websocket.repository;
 
+import learning.websocket.dto.RoomWithParticipantStatusDto;
 import learning.websocket.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
+    @Modifying
+    @Query("UPDATE Room r SET r.currentMembers = r.currentMembers + 1 WHERE r.id = :id")
+    void increaseCurrentMembers(@Param("id") Long id);
+
+    @Modifying
+    @Query("UPDATE Room r SET r.currentMembers = r.currentMembers - 1 WHERE r.id = :id")
+    void decreaseCurrentMembers(@Param("id") Long id);
+
+    @Query("SELECT new learning.websocket.dto.RoomWithParticipantStatusDto(r.id, r.roomName, r.currentMembers, r.maxMembers, CASE WHEN ur.user IS NOT NULL THEN true ELSE false END) " +
+            "FROM Room r LEFT JOIN UserRoom ur ON r.id = ur.room.id AND ur.user.id = :userId")
+    List<RoomWithParticipantStatusDto> findAllRoomsAndParticipationStatus(Long userId);
 }
