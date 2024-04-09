ebbdea772a953e87469ebd5ffa574cc334af1ffc
Algorithm <fighter0628@gmail.com>
2024-04-09 14:40:03 +0900

    feat: UserRepository 메서드 추가
    로그인 아이디로 유저 찾는 메서드 추가

--- src/main/java/learning/websocket/repository/UserRepository.java
+++ src/main/java/learning/websocket/repository/UserRepository.java
@@ -3,7 +3,9 @@ package learning.websocket.repository;
 import learning.websocket.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
-//    public Object deleteById(Long id);
+    Optional<User> findByLoginId(String loginId);
 }
