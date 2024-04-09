441403d8e02188b3be9b50a26abfd3dd034481f0
Algorithm <fighter0628@gmail.com>
2024-04-09 14:38:07 +0900

    feat: 로그인 기능 추가

--- src/main/java/learning/websocket/controller/UserController.java
+++ src/main/java/learning/websocket/controller/UserController.java
@@ -3,26 +3,37 @@ package learning.websocket.controller;
 import learning.websocket.converter.UserConverter;
 import learning.websocket.dto.UserDto;
 import learning.websocket.entity.User;
+import learning.websocket.repository.UserRepository;
 import learning.websocket.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.lang.reflect.Member;
+
+@Controller
 @RequiredArgsConstructor
-@RequestMapping("/users")
+@RequestMapping("/login")
 public class UserController {
 
     private final UserService userService;
-    private final UserConverter userConverter;
 
-    @PostMapping("/")
-    public UserDto.userRes join(@RequestBody UserDto.joinReq request) {
-        User user = userService.joinUser(request);
-        return userConverter.toUserDto(user);
+    @GetMapping("/loginForm")
+    public String loginForm() {return "login/loginForm";}
+
+    @GetMapping("/signInForm")
+    public String signInForm() {
+        return "login/signInForm";
     }
 
-    @DeleteMapping("/")
-    public void remove(@RequestBody UserDto.removeReq request) {
-        userService.removeUser(request);
+    @PostMapping("/signInForm/process")
+    public String signInProcess(
+            @RequestParam(name = "userId") String userId,
+            @RequestParam(name = "name") String name,
+            @RequestParam(name = "password") String password) {
+
+        userService.signIn(userId, name, password);
+        return "redirect:/login/loginForm";
     }
 }
