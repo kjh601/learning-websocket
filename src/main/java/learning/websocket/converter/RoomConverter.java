45bc31e2a15102897e3180c62f94e7366d024cad
Algorithm <fighter0628@gmail.com>
2024-04-09 14:23:33 +0900

    feat: RoomConverter 메서드 추가
    기존의 RoomDto가 세분화 되면서 필요한 converter 메서드 추가

--- src/main/java/learning/websocket/converter/RoomConverter.java
+++ src/main/java/learning/websocket/converter/RoomConverter.java
@@ -1,14 +1,26 @@
 package learning.websocket.converter;
 
 import learning.websocket.dto.RoomDto;
+import learning.websocket.dto.RoomWithParticipantStatusDto;
 import learning.websocket.entity.Room;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RoomConverter {
-    public Room toRoom(RoomDto.createReq req) {
+    public Room toCreateRoom(RoomDto.createReq req) {
         return Room.builder()
+                .currentMembers(0)
                 .roomName(req.getName())
+                .maxMembers(req.getMaxMembers())
+                .build();
+    }
+
+    public Room toEditRoom(RoomDto.editReq req, Long roomId, Integer currentMembers) {
+        return Room.builder()
+                .id(roomId)
+                .currentMembers(currentMembers)
+                .roomName(req.getName())
+                .maxMembers(req.getMaxMembers())
                 .build();
     }
 
@@ -16,6 +28,18 @@ public class RoomConverter {
         return RoomDto.roomRes.builder()
                 .id(room.getId())
                 .name(room.getRoomName())
+                .currentMembers(room.getCurrentMembers())
+                .maxMembers(room.getMaxMembers())
+                .build();
+    }
+
+    public RoomWithParticipantStatusDto toRoomDto(Room room, Boolean isParticipating) {
+        return RoomWithParticipantStatusDto.builder()
+                .id(room.getId())
+                .name(room.getRoomName())
+                .currentMembers(room.getCurrentMembers())
+                .maxMembers(room.getMaxMembers())
+                .isParticipating(isParticipating)
                 .build();
     }
 }
