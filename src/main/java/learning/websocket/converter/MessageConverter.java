5e29d114643a03a730d7e4fe070f0f4fdfd0909a
Algorithm <fighter0628@gmail.com>
2024-05-09 19:58:48 +0900

    feat: toLogRes 메서드 오버로딩 및 toMessageDocument 메서드 추가
    - 컨버터에 MessageDocument를 위한 메서드 추가
    - 헷갈릴 수 있는 파라미터명 변경 senderId -> userId

--- src/main/java/learning/websocket/converter/MessageConverter.java
+++ src/main/java/learning/websocket/converter/MessageConverter.java
@@ -1,5 +1,6 @@
 package learning.websocket.converter;
 
+import learning.websocket.document.MessageDocument;
 import learning.websocket.dto.MessageDto;
 import learning.websocket.entity.Message;
 import learning.websocket.entity.Room;
@@ -10,12 +11,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class MessageConverter {
 
-    public MessageDto.logRes toLogRes(Message message, Long senderId) {
+    public MessageDto.logRes toLogRes(Message message, Long userId) {
         return MessageDto.logRes.builder()
                 .senderName(message.getUser().getName())
                 .messageType(message.getMessageType().toString())
                 .createAt(message.getCreateAt())
-                .isMine(message.getUser().getId().equals(senderId))
+                .isMine(message.getUser().getId().equals(userId))
+                .message(message.getContent())
+                .build();
+    }
+
+    public MessageDto.logRes toLogRes(MessageDocument message, Long userId) {
+        return MessageDto.logRes.builder()
+                .senderName(message.getUserName())
+                .messageType(message.getMessageType().toString())
+                .createAt(message.getCreateAt())
+                .isMine(message.getUserId().equals(userId))
                 .message(message.getContent())
                 .build();
     }
@@ -24,6 +35,16 @@ public class MessageConverter {
         return Message.builder()
                 .user(user)
                 .room(room)
+                .content(saveReq.getMessage())
+                .messageType(saveReq.getMessageType())
+                .build();
+    }
+
+    public MessageDocument toMessageDocument(MessageDto.saveReq saveReq, User user, Room room) {
+        return MessageDocument.builder()
+                .userId(user.getId())
+                .userName(user.getName())
+                .roomId(room.getId())
                 .content(saveReq.getMessage())
                 .messageType(saveReq.getMessageType())
                 .build();
