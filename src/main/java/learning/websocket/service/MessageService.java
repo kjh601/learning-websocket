66fa91583042f9d11b8b10caf977a9ef2d5004f9
Algorithm <fighter0628@gmail.com>
2024-05-09 19:52:13 +0900

    refactor: MessageService 구현체를 인터페이스로 변경
    - 다양한 DB 종류에 따른 구현체를 추가할 수 있게 되었다

--- src/main/java/learning/websocket/service/MessageService.java
+++ src/main/java/learning/websocket/service/MessageService.java
@@ -1,46 +1,15 @@
 package learning.websocket.service;
 
-import jakarta.transaction.Transactional;
-import learning.websocket.converter.MessageConverter;
 import learning.websocket.dto.MessageDto;
-import learning.websocket.entity.Message;
-import learning.websocket.entity.Room;
-import learning.websocket.entity.User;
-import learning.websocket.repository.MessageRepository;
-import learning.websocket.repository.RoomRepository;
-import learning.websocket.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import learning.websocket.enums.MessageType;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-@Service
-@RequiredArgsConstructor
-public class MessageService {
+public interface MessageService {
 
-    private final MessageRepository messageRepository;
-    private final MessageConverter messageConverter;
+    void save(MessageDto.saveReq saveReq, Long roomId);
 
-    private final UserRepository userRepository;
-    private final RoomRepository roomRepository;
+    List<MessageDto.logRes> getMessages(Long roomId, Long userId);
 
-    public void save(MessageDto.saveReq saveReq, Long roomId) {
-
-        User user = userRepository.findById(saveReq.getSenderId()).orElseThrow();
-        Room room = roomRepository.findById(roomId).orElseThrow();
-
-        Message message = messageConverter.toMessage(saveReq, user, room);
-
-        messageRepository.save(message);
-    }
-
-    @Transactional
-    public List<MessageDto.logRes> getMessages(Long roomId, Long userId) {
-        Room findRoom = roomRepository.findById(roomId).orElseThrow();
-
-        return messageRepository.findAllByRoom(findRoom).stream()
-                .map(room -> messageConverter.toLogRes(room, userId))
-                .collect(Collectors.toList());
-    }
+    void sendMessage(Long userId, Long roomId, MessageType messageType);
 }
